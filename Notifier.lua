4cf68832ccdfa4ec508887f8dd20fa1bda5054b9
rhy <106398343+rhywme@users.noreply.github.com>
2024-12-13 17:57:44 +0100

    Update Notifier.lua

--- Notifier.lua
+++ Notifier.lua
@@ -1,4 +1,4 @@
-local function rhynotif(Title, Text, Icon, Duration)
+local function rhynotif(Title, Text, Icon, Duration) -- You can change the function's name to anything you want.
 game.StarterGui:SetCore("SendNotification", {
 Title = Title;
 Text = Text;
@@ -6,3 +6,7 @@ Icon = Icon;
 Duration = Duration;
 })
 end
+
+--[[ Example:
+rhynotif("Your title", "Your text", "rbxassetid://11207341665" , "5")
+]]--
